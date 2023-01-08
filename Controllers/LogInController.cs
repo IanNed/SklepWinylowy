f5f2ebe8564c81e76e7a2052d1cfbf01e29d9517
Ivan Nedilko <untrusd@gmail.com>
2023-01-08 23:07:37 +0100

    Add project files.

---
+++ Controllers/LogInController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinylStoreWebApp.Models;
+using VinylStoreWebApp.Models;
+
+namespace VinylStoreWebApp.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel dogo = new DogViewModel() { Name = "Fido", Age = 3};
+            return View(dogo);
+        }
+        public string SignUp()
+        {
+            return "Who's there?";
+        }
+        
+    }
+}
